755d450df5da46a69919ad3b02ecebcfdce6ea01
Hristo Chipev <107315850+HChipev@users.noreply.github.com>
2023-12-19 23:00:36 +0200

    Fix GoogleUserInfoViewModel property names

--- Data/ViewModels/Identity/Models/GoogleUserInfoViewModel.cs
+++ Data/ViewModels/Identity/Models/GoogleUserInfoViewModel.cs
@@ -1,11 +1,18 @@
+using Newtonsoft.Json;
+
 namespace Data.ViewModels.Identity.Models
 {
     public class GoogleUserInfoViewModel
     {
         public string Sub { get; set; }
         public string Name { get; set; }
+
+        [JsonProperty("given_name")]
         public string GivenName { get; set; }
+
+        [JsonProperty("family_name")]
         public string FamilyName { get; set; }
+
         public string Picture { get; set; }
         public string Email { get; set; }
         public bool EmailVerified { get; set; }
